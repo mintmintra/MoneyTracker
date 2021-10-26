97b81f9bbadea4af71da52d6744140f094490f37
mintmintra <mintracss019@gmail.com>
2021-10-26 21:49:39 +0700

    number formatting

--- script.js
+++ script.js
@@ -25,10 +25,16 @@ function addDataToList(transactions) {
     const symbol = transactions.amount < 0 ? '-' : '+';
     const status = transactions.amount < 0 ? 'minus' : 'plus';
     const item = document.createElement('li');
+    result = formatNumber(Math.abs(transactions.amount));
     item.classList.add(status);
-    item.innerHTML = `${transactions.text}<span>${symbol}${Math.abs(transactions.amount)}</span><button class="delete-btn">x</button>`;
+    item.innerHTML = `${transactions.text}<span>${symbol}${result}</span><button class="delete-btn">x</button>`;
     list.appendChild(item);
 }
+
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
 function calculateMoney() {
     const amounts = transactions.map(transactions => transactions.amount);
     //คำนวนยอดคงเหลือ
@@ -39,9 +45,9 @@ function calculateMoney() {
     const expense = (amounts.filter(item => item < 0).reduce((result, item) => (result += item),0)*-1).toFixed(2);
 
     //แสดงผลจอภาพ
-    balance.innerText = `฿${total}`;
-    money_plus.innerText = `฿${income}`;
-    money_minus.innerText = `฿${expense}`;
+    balance.innerText = `฿`+formatNumber(total);
+    money_plus.innerText = `฿`+formatNumber(income);
+    money_minus.innerText = `฿`+formatNumber(expense);
 }
 
 init();
